d3f908aff51ed42caf0beea1f917699b262b41c4
James DV <azyriel14@gmail.com>
2015-04-10 06:54:03 +0800

    Initial commit.

---
+++ src/querybuilder192/Main.java
@@ -0,0 +1,12 @@
+package querybuilder192;
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
