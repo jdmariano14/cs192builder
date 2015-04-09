27f188be109ef567f579a50bbfdf3d2a763c5421
James DV <azyriel14@gmail.com>
2015-04-10 07:55:28 +0800

    Write QueryBuilder class.

---
+++ src/querybuilder192/QueryBuilder.java
@@ -0,0 +1,22 @@
+package querybuilder192;
+
+/**
+ *
+ * @author MARIANO, J Stephen DV
+ * @studentno 2012-78002
+ */
+public class QueryBuilder {
+    String action;
+    String columns;
+    String from;
+    String where;
+    String group;
+    String having;
+    String as;
+    
+    public QueryBuilder() { }
+    
+    public Query build() {
+        return new Query(action, columns, from, where, group, having, as);
+    }
+}
--- test/querybuilder192/MainTest.java
+++ test/querybuilder192/MainTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package querybuilder192;
 
 import org.junit.After;
@@ -12,10 +7,6 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author JaymNico
- */
 public class MainTest {
     
     public MainTest() {
